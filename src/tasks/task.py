528c7943b8316882857dcebf420664f76ad43163
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-08-20 02:38:42 +0300

    all 3 tasks created

--- src/tasks/task.py
+++ src/tasks/task.py
@@ -1,26 +1,40 @@
-from celery.app import Celery
-from time import sleep
-from datetime import datetime
-import os
+from src.tasks import celery_app
+from commons.tasklib.base_task import BaseTask
 
 
-# config
-redis_url = os.getenv('REDIS_URL', 'redis://default:redispw@localhost:32768')
-app = Celery(__name__, broker=redis_url, backend=redis_url)
+# plan all
+@celery_app.task(
+    bind=True,
+    name='oopsdated.plan'
+)
+def plan_repos(self: BaseTask):
+    """
+    __summary__: get tasks from db and make them ready to scrape
+    :param self:
+    :return:
+    """
+    pass
 
 
 
-@app.task
-def dummy_task(name='bekocan') -> str:
-    # return open('tmp/celery/x.txt', 'w')
-
-    # sleep(5)
-    # return f'hello {name}'
 
 
-    folder = '/tmp/celery'
-    os.makedirs(folder, exist_ok=True)
-    sleep(5)
-    now = datetime.now().strftime("%Y-%m-%dT%H:%M:%s")
-    with open(f'{folder}/task-{now}.txt', 'w') as f:
-        f.write(f'hello {name}: {now}')
+# scan
+@celery_app.task(
+    bind=True,
+    name='oopsdated.scan'
+)
+def scrape_repo(
+        self: BaseTask,
+):
+    pass
+
+
+@celery_app.task(
+    bind=True,
+    name='oopsdated.extract'
+)
+def extract_repo(self: BaseTask):
+    pass
+
+
