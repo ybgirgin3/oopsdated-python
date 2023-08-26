c3c6d464c1c74eb54f0854d00e2421953800c178
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-08-26 08:29:01 +0300

    fastapi graphql implementation is done

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# from fastapi import FastAPI
+# import strawberry
+# from strawberry.fastapi import GraphQLRouter
+#
+# # import schemas here
+#
+# graphql_app = GraphQLRouter()
+# app = FastAPI()
+#
+# app.include_router(graphql_app, prefix="/graphql")
 from fastapi import FastAPI
 
+from ariadne.asgi import GraphQL
+from src.repo import repo_schema
+
+# Mount Ariadne GraphQL as sub-application for FastAPI
 app = FastAPI()
+
+app.mount("/graphql/", GraphQL(repo_schema, debug=True))
