1b580cad6eb42c8cc4dbe33d30686e0071d7c0ff
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-08-26 10:26:31 +0300

    adding mutatiions for repo

--- src/repo/resolvers.py
+++ src/repo/resolvers.py
@@ -1,26 +1,26 @@
-from ariadne import ObjectType, QueryType
+from typing import Dict
+
+from ariadne import ObjectType, QueryType, MutationType
+
+from src.commons.db import DBProcess
+from src.models import repo
+
+dbp = DBProcess(model=repo.Repo)
 
 # Map resolver functions to Query fields using QueryType
 query = QueryType()
-
+mutation = MutationType()
 
 # Map resolver functions to custom type fields using ObjectType
 repo = ObjectType("Repo")
 
 
-# alias for queries
-@repo.field("fullname")
-def resolve_repo_fullname(repo, *_):
-    return "%s/%s" % (repo["owner"], repo["name"])
-
-
-# seed
-@query.field("repos")
+# ---- QUERIES
+@query.field("repo")
 def resolve_repo(*_):
-    return [
-        {
-            "name": "react-native",
-            "owner": "Facebook",
-            "url": "https://www.github.com/facebook/react-native",
-        },
-    ]
+    return dbp.read()
+
+
+# ---- MUTATIONS
+def resolver_create(_, data: Dict):
+    return dbp.create(data=data)
