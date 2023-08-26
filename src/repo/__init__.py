ea7709bcf0812f4992bbe57e4c705ec8777048f3
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-08-26 10:26:23 +0300

    adding mutations

--- src/repo/__init__.py
+++ src/repo/__init__.py
@@ -1,12 +1,11 @@
-from ariadne import make_executable_schema
 from ariadne import load_schema_from_path
+from ariadne import make_executable_schema
 
-
-from src.repo.resolvers import query, repo
+from src.repo.resolvers import query, mutation, repo
 
 type_defs = load_schema_from_path("src/graphql")
 
 
 # Create executable GraphQL schema
 # repo_schema = make_executable_schema(type_defs, query, repo)
-repo_schema = make_executable_schema(type_defs, query, repo)
+repo_schema = make_executable_schema(type_defs, [query, mutation], repo)
