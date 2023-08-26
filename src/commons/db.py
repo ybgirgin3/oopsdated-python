94ae5ba5afe565b033fa516e384b5e2f97f237fd
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-08-26 18:53:36 +0300

    return types added

--- src/commons/db.py
+++ src/commons/db.py
@@ -2,7 +2,7 @@ import datetime
 from typing import Union, Dict
 
 from src.commons import session
-from src.models import repo, user
+from src.models import repo as m_repo, user as m_user
 
 
 class DBProcess:
@@ -18,25 +18,47 @@ class DBProcess:
 
     # m = {"repo": repo.Repo, "user": user.User}
 
-    def __init__(self, model: Union[repo.Repo, user.User]) -> None:
+    def __init__(self, model: Union[m_repo.Repo, m_user.User]) -> None:
         self.model = model
 
-    def read(self, filters: Dict = None):
+    def read(self, filters: dict = True) -> Union[m_repo, m_user]:
         with session() as sess:
             # create object from json
-            _where = True if not filters else filters
-            result = sess.query(self.model).where(_where).all()
+
+            result = sess.query(self.model).where(filters).all()
+
             return result
 
-    def create(self, data: Dict):
+    def create(self, data: dict) -> Union[m_repo, m_user]:
         with session() as sess:
             try:
                 now = datetime.datetime.today()
                 data["created_at"] = now
                 data["updated_at"] = now
+
                 obj = self.model(**data)
                 sess.add(obj)
                 sess.commit()
-                return data
+
+                return obj
+            except Exception as e:
+                raise e
+
+    def update(self, id: int, data: dict) -> Union[m_repo, m_user]:
+        with session() as sess:
+            try:
+                # find one
+                # url must be unique
+                result = self.read(filters="")
+
+                data["updated_at"] = datetime.datetime.today()
+                result.update(data)
+
+                obj = self.model(**data)
+                sess.add(obj)
+                sess.commit()
+
+                return obj
+
             except Exception as e:
                 raise e
