b56d3a37baba8de239c081bd5ce342b6cfd1a9ab
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-08-30 19:11:54 +0300

    unnecessary lines in main deleted

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-# from fastapi import FastAPI
-# import strawberry
-# from strawberry.fastapi import GraphQLRouter
-#
-# # import schemas here
-#
-# graphql_app = GraphQLRouter()
-# app = FastAPI()
-#
-# app.include_router(graphql_app, prefix="/graphql")
 from ariadne.asgi import GraphQL
 from fastapi import FastAPI
 
 from src.repo import repo_schema
 
-# Mount Ariadne GraphQL as sub-application for FastAPI
 app = FastAPI()
 
 app.mount("/graphql/", GraphQL(repo_schema, debug=True))
