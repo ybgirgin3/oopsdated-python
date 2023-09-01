eec142c75ed86f51d75fc6f8616476bdff086473
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-09-02 01:43:29 +0300

    _create_table is sits in main with try except

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from ariadne.asgi import GraphQL
 from fastapi import FastAPI
 
 from src.repo import repo_schema
+from src.commons import _create_table
+
+try:
+    _create_table()
+except Exception as e:
+    pass
 
 app = FastAPI()
 
